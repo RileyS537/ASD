9d95a0264908fa0aa37472d43de14ffe49683463
linusgo <517281575@qq.com>
2020-08-07 17:15:23 +0800

    Add files via upload

---
+++ multi_model.py
@@ -0,0 +1,130 @@
+import numpy as np
+import gc
+
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Flatten, Lambda, GaussianNoise
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.convolutional import Conv2D, Conv1D
+from keras.layers import Concatenate
+from keras.layers import Dropout
+from keras import regularizers
+
+from keras.callbacks import ModelCheckpoint
+from keras.utils import plot_model
+
+from keras.layers.recurrent import LSTM
+from keras.layers import TimeDistributed
+from keras import backend as K
+
+import tensorflow as tf
+
+from keras.utils import multi_gpu_model, plot_model
+
+def multi_kernel_no_lstm(sample_pic_length, eye_movement, nclasses):  # 350, 900->300, 4
+
+    inputs = Input(shape=(sample_pic_length, eye_movement, 4))        # 350 900->300, 4
+
+    x1 = Conv2D(32, (1, 5), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x1_dropout = Dropout(0.7)(x1)
+    average_pool1 = AveragePooling2D((1, 5), strides=(1, 5))(x1_dropout)
+    x2 = Conv2D(32, (1, 10), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x2_dropout = Dropout(0.7)(x1)
+    average_pool2 = AveragePooling2D((1, 5), strides=(1, 5))(x2_dropout)
+    x3 = Conv2D(32, (1, 20), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x3_dropout = Dropout(0.7)(x1)
+    average_pool3 = AveragePooling2D((1, 5), strides=(1, 5))(x3_dropout)
+    x4 = Conv2D(32, (1, 30), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x4_dropout = Dropout(0.7)(x1)
+    average_pool4 = AveragePooling2D((1, 5), strides=(1, 5))(x4_dropout)
+
+    append_all = Concatenate(axis=-1)([average_pool1, average_pool2, average_pool3, average_pool4])
+
+    conv_channel1 = Conv2D(16, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(append_all)
+    conv_dropout1 = Dropout(0.7)(conv_channel1)
+    conv_channel2 = Conv2D(1, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(conv_dropout1)
+    conv_dropout2 = Dropout(0.7)(conv_channel2)
+
+    flat = Flatten()(conv_dropout2)
+    fc1 = Dense(80, activation="relu")(flat)
+    fc1_drop = Dropout(0.7)(fc1)
+    fc2 = Dense(nclasses, activation="softmax")(fc1_drop)
+
+    model = Model(inputs=inputs, outputs=fc2)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    plot_model(model, to_file='multi_kernel_no_lstm.png')
+    print(model.summary())
+    return model
+
+def multi_kernel_no_lstm_gpu1(sample_pic_length, eye_movement, nclasses, lstm_output=False):
+    inputs = Input(shape=(sample_pic_length, eye_movement, 4))        # 350 900->300, 4
+
+    x1 = Conv2D(32, (1, 5), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x1_dropout = Dropout(0.7)(x1)
+    average_pool1 = AveragePooling2D((1, 5), strides=(1, 5))(x1_dropout)
+    x2 = Conv2D(32, (1, 10), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x2_dropout = Dropout(0.7)(x1)
+    average_pool2 = AveragePooling2D((1, 5), strides=(1, 5))(x2_dropout)
+    x3 = Conv2D(32, (1, 20), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x3_dropout = Dropout(0.7)(x1)
+    average_pool3 = AveragePooling2D((1, 5), strides=(1, 5))(x3_dropout)
+    x4 = Conv2D(32, (1, 30), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x4_dropout = Dropout(0.7)(x1)
+    average_pool4 = AveragePooling2D((1, 5), strides=(1, 5))(x4_dropout)
+
+    append_all = Concatenate(axis=-1)([average_pool1, average_pool2, average_pool3, average_pool4])
+
+    conv_channel1 = Conv2D(16, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(append_all)
+    conv_dropout1 = Dropout(0.7)(conv_channel1)
+    conv_channel2 = Conv2D(1, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(conv_dropout1)
+    conv_dropout2 = Dropout(0.7)(conv_channel2)
+
+    flat = Flatten()(conv_dropout2)
+    fc1 = Dense(80, activation="relu")(flat)
+    fc1_drop = Dropout(0.7)(fc1)
+    fc2 = Dense(nclasses, activation="softmax")(fc1_drop)
+
+    model = Model(inputs=inputs, outputs=fc2)
+
+    print(model.summary())
+    return model
+
+def multi_kernel_no_lstm_gpu2(sample_pic_length, eye_movement, nclasses, lstm_output=False):
+    inputs = Input(shape=(sample_pic_length, eye_movement, 4))        # 350 900->300, 4
+
+    x1 = Conv2D(32, (1, 10), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x1_dropout = Dropout(0.7)(x1)
+    average_pool1 = AveragePooling2D((1, 10), strides=(1, 5))(x1_dropout)
+    x2 = Conv2D(32, (1, 20), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x2_dropout = Dropout(0.7)(x2)
+    average_pool2 = AveragePooling2D((1, 10), strides=(1, 5))(x2_dropout)
+    x3 = Conv2D(32, (1, 30), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x3_dropout = Dropout(0.7)(x3)
+    average_pool3 = AveragePooling2D((1, 10), strides=(1, 5))(x3_dropout)
+    x4 = Conv2D(32, (1, 40), padding='same', kernel_regularizer=regularizers.l1(0.), activation='relu')(inputs)
+    x4_dropout = Dropout(0.7)(x4)
+    average_pool4 = AveragePooling2D((1, 10), strides=(1, 5))(x4_dropout)
+
+    append_all = Concatenate(axis=-1)([average_pool1, average_pool2, average_pool3, average_pool4])
+
+    conv_channel1 = Conv2D(16, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(append_all)
+    conv_dropout1 = Dropout(0.7)(conv_channel1)
+    conv_channel2 = Conv2D(1, (1, 1), kernel_regularizer=regularizers.l1(0.), activation='relu')(conv_dropout1)
+    conv_dropout2 = Dropout(0.7)(conv_channel2)
+
+    flat = Flatten()(conv_dropout2)
+    fc1 = Dense(128, activation="relu")(flat)
+    fc1_drop = Dropout(0.7)(fc1)
+    fc2 = Dense(nclasses, activation="softmax")(fc1_drop)
+
+    model = Model(inputs=inputs, outputs=fc2)
+    plot_model(model, to_file='multi_kernel_no_lstm.png')
+
+    print(model.summary())
+    return model
+
+
+
+
+# create_raw_model2(64, 2)
+# multi_kernel_no_lstm(350, 300, 2)
+# multi_kernel_with_lstm(350, 300, 2)
---
+++ train_multi_kernel.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os
+import random
+import scipy.io
+import multi_model
+import keras
+
+from keras.callbacks import ModelCheckpoint, TensorBoard
+import tensorflow as tf
+from keras.callbacks import Callback
+from keras.utils import multi_gpu_model
+from keras import optimizers
+
+MODEL_PATH = '/home/amax/DL/sun/asd/multi_kernel_model/'
+HIS_PATH = '/home/amax/DL/sun/asd/multi_kernel_model/hist/'
+TOTAL_PERSON = 39
+TRAIN_PERSON_NUMBER = 33
+TEST_PERSON_NUMBER = 6
+
+nclasses = 2
+sample_num = 400
+pic_per_sample = 350
+eye_movement_length = 300
+nchann = 4
+epoch = 2
+
+# use multi gpu training
+os.environ["CUDA_VISIBLE_DEVICES"]='4,5,6'
+config = tf.ConfigProto()
+
+# use all gpus available (defined by: CUDA_VISIBLE_DEVICES)
+def get_available_gpus():
+    from tensorflow.python.client import device_lib as _device_lib 
+    local_device_protos = _device_lib.list_local_devices()
+    return len([x.name for x in local_device_protos if x.device_type == 'GPU'])
+
+num_gpu = get_available_gpus()
+
+# print the prediction
+class CollectOutputAndTarget(Callback):
+    def __init__(self):
+        super(CollectOutputAndTarget, self).__init__()
+        self.targets = []  #
+        self.outputs = []  # collect y_pred batches
+
+        # the shape of these 2 variables will change according to batch shape
+        # to handle the "last batch", specify `validate_shape=False`
+        self.var_y_true = tf.Variable(0., validate_shape=False)
+        self.var_y_pred = tf.Variable(0., validate_shape=False)
+
+    def on_epoch_end(self, batch, logs=None):
+        # evaluate the variables and save them into lists
+        self.targets.append(K.eval(self.var_y_true))
+        self.outputs.append(K.eval(self.var_y_pred))
+
+# multi GPU save model
+class CustomModelCheckpoint(keras.callbacks.Callback):
+
+    def __init__(self, model, path):
+        self.model = model
+        self.path = path
+        self.best_loss = np.inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        val_loss = logs['val_loss']
+        if val_loss < self.best_loss:
+            print("\nValidation loss decreased from {} to {}, saving model".format(self.best_loss, val_loss))
+            self.model.save_weights(self.path, overwrite=True)
+            self.best_loss = val_loss
+
+
+# load data
+X = np.load('/home/amax/DL/sun/asd/train_data_noise2.npy')   # (13200, 350, 300, 4)  33
+Y = np.load('/home/amax/DL/sun/asd/train_label_noise2.npy')  # (13200, 2)    
+testX = np.load('/home/amax/DL/sun/asd/test_data_noise2.npy')     # (2400, 350, 300, 4)    6
+testY = np.load('/home/amax/DL/sun/asd/test_label_noise2.npy')    # (2400, 2)
+trainX = X[:31*400]
+trainY = Y[:31*400]
+valX = X[31*400:]    #  (1200, 350, 300, 4) 2
+valY = Y[31*400:]    #  (1200, 2)   2
+# shuffle validation data, shuffle data with same state
+state = np.random.get_state()
+np.random.shuffle(valX)
+np.random.set_state(state)
+np.random.shuffle(valY)
+
+
+# training  http://www.manongjc.com/article/31408.html
+
+model = multi_model.multi_kernel_no_lstm_gpu2(pic_per_sample, eye_movement_length, nclasses)
+mgpu_model = multi_gpu_model(model, gpus=num_gpu)
+
+# opt = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.01)
+# mgpu_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['acc'])
+
+mgpu_model.compile(loss='categorical_crossentropy', optimizer='Adadelta', metrics=['acc'])
+cbk = CollectOutputAndTarget()
+
+fetches = [tf.assign(cbk.var_y_pred, mgpu_model.outputs[0], validate_shape=False)]
+mgpu_model._function_kwargs = {'fetches': fetches}
+
+checkpoint = ModelCheckpoint(filepath=os.path.join(MODEL_PATH, 'model_real_multi_kernel-{epoch:02d}.h5'))
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+
+hist = mgpu_model.fit(X, Y, batch_size=128, epochs=epoch, verbose=1, callbacks=[checkpoint, tensorboard], 
+         shuffle=True, validation_data=(valX, valY))
+
+result_val_loss = hist.history['val_loss']
+result_val_acc = hist.history['val_acc']
+
+print(len(result_val_loss))
+print(len(result_val_acc))
+print(result_val_loss)
+print(result_val_acc)
+print(np.array(result_val_loss).shape)
+
+mgpu_model.save('real_multi_kernel2.h5')
+
+# testing
+score = mgpu_model.evaluate(testX, testY, verbose=1)
+print('test accuracy:', score[1])
+
+
